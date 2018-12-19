72f6308a5b196734a768cd736794c576eba237e1
ZM <443624617@qq.com>
2018-12-19 11:41:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zm/testvideodemo/MainActivity.java
@@ -0,0 +1,202 @@
+package com.example.zm.testvideodemo;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ProgressBar progressBar;
+
+//    private String baseUrl = "https://www.iqiyi.com/v_19rr4phdzo.html";//爱奇艺
+    private String baseUrl = "http://123.125.218.24:28080/login";
+
+
+//    private String baseUrl = "http://139.196.253.185/808gps/open/player/video.html?lang=zh&devIdno=31850&account=junyang&password=000000";
+
+//    private String baseUrl="http://139.196.253.185:6604/hls/1_10000_0_1.m3u8?JSESSIONID=cf6b70a3-c82b-4392-8ab6-bbddce336222";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boolean isCheck = check();
+
+        Log.e("======", "====isCheck====" + isCheck);
+        if (!isCheck) {
+            Intent installIntent = new Intent(
+                    "android.intent.action.VIEW");
+            installIntent.setData(Uri
+                    .parse("market://details?id=com.adobe.flashplayer"));
+            startActivity(installIntent);
+        }
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);//进度条
+
+        webView = (WebView) findViewById(R.id.webview);
+//        webView.loadUrl("file:///android_asset/test.html");//加载asset文件夹下html
+        webView.loadUrl(baseUrl);//加载url
+
+        webView.addJavascriptInterface(this, "android");//添加js监听 这样html就能调用客户端
+        webView.setWebChromeClient(webChromeClient);
+
+        webView.setWebViewClient(webViewClient);
+
+        WebSettings webSettings = webView.getSettings();
+
+
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setGeolocationEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setUseWideViewPort(true); // 关键点
+        webSettings.setAllowFileAccess(true); // 允许访问文件
+        webSettings.setSupportZoom(true); // 支持缩放
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 不加载缓存内容
+
+
+        /**
+         * LOAD_CACHE_ONLY: 不使用网络，只读取本地缓存数据
+         * LOAD_DEFAULT: （默认）根据cache-control决定是否从网络上取数据。
+         * LOAD_NO_CACHE: 不使用缓存，只从网络获取数据.
+         * LOAD_CACHE_ELSE_NETWORK，只要本地有，无论是否过期，或者no-cache，都使用缓存中的数据。
+         */
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);//不使用缓存，只从网络获取数据.
+
+        //支持屏幕缩放b
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        //不显示webview缩放按钮
+//        webSettings.setDisplayZoomControls(false);
+    }
+
+    //WebViewClient主要帮助WebView处理各种通知、请求事件
+    private WebViewClient webViewClient = new WebViewClient() {
+        @Override
+        public void onPageFinished(WebView view, String url) {//页面加载完成
+            progressBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {//页面开始加载
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.i("ansen", "拦截url:" + url);
+            if (url.equals("http://www.google.com/")) {
+                Toast.makeText(MainActivity.this, "国内不能访问google,拦截该url", Toast.LENGTH_LONG).show();
+                return true;//表示我已经处理过了
+            }
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    };
+
+
+    private boolean check() {
+        PackageManager pm = getPackageManager();
+        List<PackageInfo> infoList = pm
+                .getInstalledPackages(PackageManager.GET_SERVICES);
+        for (PackageInfo info : infoList) {
+            if ("com.adobe.flashplayer".equals(info.packageName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    //WebChromeClient主要辅助WebView处理Javascript的对话框、网站图标、网站title、加载进度等
+    private WebChromeClient webChromeClient = new WebChromeClient() {
+        //不支持js的alert弹窗，需要自己监听然后通过dialog弹窗
+        @Override
+        public boolean onJsAlert(WebView webView, String url, String message, JsResult result) {
+            AlertDialog.Builder localBuilder = new AlertDialog.Builder(webView.getContext());
+            localBuilder.setMessage(message).setPositiveButton("确定", null);
+            localBuilder.setCancelable(false);
+            localBuilder.create().show();
+
+            //注意:
+            //必须要这一句代码:result.confirm()表示:
+            //处理结果为确定状态同时唤醒WebCore线程
+            //否则不能继续点击按钮
+            result.confirm();
+            return true;
+        }
+
+        //获取网页标题
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+            Log.i("ansen", "网页标题:" + title);
+        }
+
+        //加载进度回调
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            progressBar.setProgress(newProgress);
+        }
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.i("ansen", "是否有上一个页面:" + webView.canGoBack());
+        if (webView.canGoBack() && keyCode == KeyEvent.KEYCODE_BACK) {//点击返回按钮的时候判断有没有上一页
+            webView.goBack(); // goBack()表示返回webView的上一页面
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * JS调用android的方法
+     *
+     * @param str
+     * @return
+     */
+    @JavascriptInterface //仍然必不可少
+    public void getClient(String str) {
+        Log.i("ansen", "html调用客户端:" + str);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //释放资源
+        webView.destroy();
+        webView = null;
+    }
+}
